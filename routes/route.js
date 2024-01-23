e787b2573410d06e0c0d5fb1335e46a1f1eaa39c
Anny Stecca <anny23sa@gmail.com>
2024-01-22 21:33:42 -0300

    feature:se agrego el endpoint a routes.js

--- routes/route.js
+++ routes/route.js
@@ -12,10 +12,23 @@ router.get('/canciones', (req,res)=>{
     const repertorio = JSON.parse(fs.readFileSync("./repertorio.json", "utf8"));
     res.json(repertorio)
 })
+
+router.put('/canciones/:id', (req, res)=>{
+    const {id} = req.params
+    const cancion = req.body
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json"))
+    const index = canciones.findIndex(p => p.id == id)
+    canciones[index] = cancion
+    fs.writeFileSync("repertorio.json", JSON.stringify(canciones))
+       res.send("Cancion modificada con éxito")
+})
+
+
 //router.get("/", (req, res) => {
 //    res.send("Hello World! desde express con routes");
 //  });
-  
+
+
 
 module.exports = router;
 
